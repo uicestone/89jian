80f81d092f9378f1ac22f89b20a51d0115886e02
supersheep <supersheepxjy@gmail.com>
2014-01-06 21:52:00 +0800

    完成字體加載器

---
+++ view-prototype/js/fontloader.js
@@ -0,0 +1,34 @@
+var FontLoader = (function(){
+
+    function load(paths){
+        var count = paths.length;
+        function checkDone(){
+            count--;
+            if(count == 0){
+                $("<link />", {
+                    'rel': 'stylesheet',
+                    'href': '/css/font.css'
+                }).appendTo('head');
+                document.cookie = "fontloaded=1";
+            }
+        }
+
+        for(var i = 0; i < count; i++){            
+            $.ajax({
+                url: "fonts/" + paths[i],
+                beforeSend: function ( xhr ) {
+                  xhr.overrideMimeType("font/opentype");
+                },
+                success: checkDone
+            });   
+        }
+    }
+
+    return {
+        load:load
+    }
+})();
+
+
+FontLoader.load(["TpldKhangXiDictTrial.otf","TrajanPro-Bold.otf","TrajanPro-Regular.otf"]);
+
