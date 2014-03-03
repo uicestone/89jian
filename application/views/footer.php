a2395f06079925332b9e047d9a85025c0adf855c
陆秋石 <uicestone@gmail.com>
2014-03-04 03:11:15 +0800

    页面底部文章链接

--- application/views/footer.php
+++ application/views/footer.php
@@ -18,41 +18,41 @@
                 <li class="group span3">
                     <div class="title">食品安全</div>
                     <ul class="links">
-                        <li><a href="#">原生態食材選擇指南</a></li>
-                        <li><a href="#">為何選擇原生態食品</a></li>
+                        <li><a href="/article/原生态食材选择指南">原生態食材選擇指南</a></li>
+                        <li><a href="/article/为何选择原生态食品">為何選擇原生態食品</a></li>
                     </ul>
                 </li>
                 <li class="group span2">
                     <div class="title">新手指南</div>
                     <ul class="links">
-                        <li><a href="#">會員註冊</a></li>
-                        <li><a href="#">購物流程</a></li>
-                        <li><a href="#">會員手冊</a></li>
+                        <li><a href="/article/会员注册">會員註冊</a></li>
+                        <li><a href="/article/购物流程">購物流程</a></li>
+                        <li><a href="/article/会员手册">會員手冊</a></li>
                     </ul>
                 </li>
                 <li class="group span2">
                     <div class="title">支付方式</div>
                     <ul class="links">
-                        <li><a href="#">在綫支付</a></li>
-                        <li><a href="#">貨到付款</a></li>
-                        <li><a href="#">發票制度</a></li>
+                        <li><a href="/article/在线支付">在綫支付</a></li>
+                        <li><a href="/article/货到付款">貨到付款</a></li>
+                        <li><a href="/article/发票制度">發票制度</a></li>
                     </ul>
                 </li>
                 <li class="group span3">
                     <div class="title">配送說明</div>
                     <ul class="links">
-                        <li><a href="#">冷鏈配送</a></li>
-                        <li><a href="#">上門自提</a></li>
-                        <li><a href="#">配送範圍與費用</a></li>
+                        <li><a href="/article/冷链配送">冷鏈配送</a></li>
+                        <li><a href="/article/上门自提">上門自提</a></li>
+                        <li><a href="/article/配送范围与费用">配送範圍與費用</a></li>
                     </ul>
                 </li>
                 <li class="group span2">
                     <div class="title">幫助中心</div>
                     <ul class="links">
-                        <li><a href="#">退換貨說明</a></li>
-                        <li><a href="#">找回密碼</a></li>
-                        <li><a href="#">常見問題</a></li>
-                        <li><a href="#">聯系我們</a></li>
+                        <li><a href="/article/退换货说明">退換貨說明</a></li>
+                        <li><a href="/article/找回密码">找回密碼</a></li>
+                        <li><a href="/article/常见问题">常見問題</a></li>
+                        <li><a href="/article/联系我们">聯系我們</a></li>
                     </ul>
                 </li>
             </ul>
