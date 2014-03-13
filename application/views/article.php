18d6ec242fa629bc57fc70385c29a20f10a58f59
陆秋石 <uicestone@gmail.com>
2014-03-13 15:20:58 +0800

    文章页中添加一个按钮来显示标准字体

--- application/views/article.php
+++ application/views/article.php
@@ -1,6 +1,15 @@
 <?php $this->view('header'); ?>
 <div class="article-page">
+	<a type="button" id="font-standard" class="btn pull-right">标准字体</a>
 	<h1><?=$article['name']?></h1>
 	<?=end($article['meta']['内容'])?>
 </div>
+<script type="text/javascript">
+$(function(){
+	$('#font-standard').on('click', function(){
+		$('.article-page').css('font-family', 'STZhongsong, Songti SC, SimSun, serif');
+		$(this).fadeOut(100);
+	});
+});
+</script>
 <?php $this->view('footer'); ?>
