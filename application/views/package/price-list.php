60999f8a36666c90d9ee1699598afb65b5336e12
Uice Lu <uicestone@gmail.com>
2014-06-03 17:43:54 +0800

    套餐展示

--- application/views/package/price-list.php
+++ application/views/package/price-list.php
@@ -1,5 +1,15 @@
 <?php $this->view('header'); ?>
-<div class="price-tag"><a href="<?=site_url()?>package/A">套餐A</a></div>
-<div class="price-tag"><a href="<?=site_url()?>package/B">套餐B</a></div>
-<div class="price-tag"><a href="<?=site_url()?>package/C">套餐C</a></div>
+<div class="article-page" style="padding:0">
+	<div class="price-tag">
+		<a href="<?=site_url()?>package/A"><img src="<?=site_url()?>img/package-A.jpg" alt="套餐A"></a>
+	</div>
+	<hr>
+	<div class="price-tag">
+		<a href="<?=site_url()?>package/B"><img src="<?=site_url()?>img/package-B.jpg" alt="套餐B"></a>
+	</div>
+	<hr>
+	<div class="price-tag">
+		<a href="<?=site_url()?>package/C"><img src="<?=site_url()?>img/package-C.jpg" alt="套餐C"></a>
+	</div>
+</div>
 <?php $this->view('footer'); ?>
--- application/views/package/type-list.php
+++ application/views/package/type-list.php
@@ -1,8 +1,12 @@
 <?php $this->view('header'); ?>
+<div class="container" style="padding:30px">
 <?php foreach($packages['data'] as $package){ ?>
-<div>
-	<h1><?=$package['name']?></h1>
-	<div><?=get_meta($package, '组成')?></div>
-</div>
+	<div style="display:inline-block;vertical-align:top;border:3px #1e2f36 double;padding:30px;margin:20px">
+		<h1><?=$package['name']?></h1>
+		<div class="text-right"><?=get_tag($package, '内容分类')?> ¥<?=get_meta($package, '价格')?></div>
+		<hr>
+		<div><?=get_meta($package, '组成')?></div>
+	</div>
 <? } ?>
+</div>
 <?php $this->view('footer'); ?>
